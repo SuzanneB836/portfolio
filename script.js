3ad24082378bd23d019ee87db82af66ea798271e
SuzanneB836 <6035697@mborijnland.nl>
2024-07-21 15:25:01 +0200

    upload

--- script.js
+++
@@ -1,8 +0,0 @@
-function expand() {
-    var x = document.getElementById("iframe");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
